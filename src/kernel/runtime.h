a2762376bfba86eb3ec1c3cbd7a60013664c1981
surrealtm <matheke.victor@gmail.com>
2026-09-23 21:39:45 +0200

    Added a custom unsigned 64 bit integer implementation.

---
+++ src/kernel/runtime.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * Implements division of two unsigned 64 bit integers.
+ * The X86 instruction set does not support this natively, so compilers usually provide their own
+ * implementation.
+ * Since we're compiling in free-standing mode, we have to provide our own division function.
+ */
+u64 __udivdi3(u64 divident, u64 divisor);
