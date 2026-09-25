62314c480257b676d693c0776cce1b5eb4e01159
surrealtm <matheke.victor@gmail.com>
2026-09-25 21:40:49 +0200

    Parsing input events from the keyboard interrupt.

--- include/input.h
+++ include/input.h
@@ -6,8 +6,23 @@ typedef enum OS_Input_Event_Kind {
     OS_INPUT_EVENT_KIND_Keyboard,
 } OS_Input_Event_Kind;
 
+typedef enum OS_Input_Key_Code {
+    OS_INPUT_KEY_Escape,
+    OS_INPUT_KEY_Enter,
+    OS_INPUT_KEY_Space,
+} OS_Input_Key_Code;
+
+typedef struct OS_Input_Keyboard_Event {
+    OS_Input_Key_Code key_code;
+    b8 down;
+} OS_Input_Keyboard_Event;
+
 typedef struct OS_Input_Event {
     OS_Input_Event_Kind kind;
+
+    union data {
+        OS_Input_Keyboard_Event keyboard;
+    } data;
 } OS_Input_Event;
 
 b8 os_input_has_event(void);
--- src/input/input.c
+++ src/input/input.c
@@ -3,10 +3,39 @@
 #include "interrupt/interrupt.h"
 #include "port/port.h"
 
+#define SUPPORTED_SCAN_CODE_COUNT 0x39
+
+static
+OS_Input_Key_Code scan_code_table[SUPPORTED_SCAN_CODE_COUNT] = {
+    0xff, // A scan code of 0 represents an error
+    OS_INPUT_KEY_Escape,
+};
+
+static
+OS_Input_Event make_keyboard_event(const OS_Input_Keyboard_Event keyboard) {
+    OS_Input_Event event;
+    event.kind = OS_INPUT_EVENT_KIND_Keyboard;
+    event.data.keyboard = keyboard;
+    return event;
+}
+
+static
+void push_event(const OS_Input_Event event) {
+    (void) event;
+}
+
 static
 void keyboard_interrupt_handler(void) {
+    const u8 down_indicator = 0x80;
     const u8 scan_code = port_read_u8(0x60);
-    (void) scan_code;
+    const b8 down = scan_code >= down_indicator;
+    const u8 normalized_scan_code = (down) ? scan_code - down_indicator : scan_code;
+
+    if(normalized_scan_code > 0 && normalized_scan_code < ARRAY_COUNT(scan_code_table)) {
+        const OS_Input_Key_Code key_code = scan_code_table[normalized_scan_code];
+        const OS_Input_Keyboard_Event keyboard_event = (OS_Input_Keyboard_Event) { key_code, down };
+        push_event(make_keyboard_event(keyboard_event));
+    }
 }
 
 void input_initialize(void) {
